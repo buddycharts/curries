24f5a2dc8f37bb5752216454bc3b9b2a8b33a8ec
buddycharts <arjunreddy356690@gmail.com>
2025-11-19 11:53:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 const app = express();
 
-app.use(cors());
-
-// Serve curry images WITHOUT public folder
-app.use("/curry", express.static(path.join(__dirname, "curry")));
-
-const data = require("./curry.json");
+// serve everything inside the "curry" folder
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.send("Curry API Running Successfully!");
-});
-
-app.get("/curries", (req, res) => {
-  res.json(data);
+  res.send("Curries API Working!");
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on ${port}`));
+app.listen(port, () => console.log("Server running on port " + port));
