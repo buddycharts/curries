693b0c114514bf6b06159b73fc9a6e609305fc13
buddycharts <arjunreddy356690@gmail.com>
2025-11-19 12:28:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// serve everything inside the "curry" folder
+// --- FIX CORS ISSUE ---
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  next();
+});
+
+// Serve all files in this folder (your curry folder)
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.send("Curries API Working!");
+  res.send("Curries API Working with CORS!");
 });
 
 const port = process.env.PORT || 3000;
